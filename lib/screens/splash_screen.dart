fb997c854c41b2cb5632dba2c6331f3260ea4aae
Lojaen3 <lojaenayash@gmail.com>
2025-02-12 15:42:51 +0300

    fourth commit

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(const Duration(seconds: 3), () {
-      // Navigate to login Screen after 3 seconds
+      // Navigate to login screen after 3 seconds
       Navigator.pushReplacementNamed(context, '/login');
     });
   }
@@ -23,20 +23,64 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
-          color: Color.fromRGBO(
-              255, 255, 255, 1), // Set background color to pure white
+          color: Color.fromRGBO(255, 255, 255, 1),
         ),
         child: Stack(
           children: [
+            /// Top Circular Effect
+            Positioned(
+              left: 20,
+              top: -305,
+              child: Container(
+                width: 356.96,
+                height: 356.96,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color.fromRGBO(122, 29, 255, 0.4),
+                      blurRadius: 225.88,
+                      spreadRadius: 0,
+                      offset: const Offset(20, 20),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            /// Bottom Circular Effect
+            Positioned(
+              right: -100,
+              bottom: -300,
+              child: Container(
+                width: 356.96,
+                height: 356.96,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color.fromRGBO(42, 210, 201, 0.8),
+                      blurRadius: 225.88,
+                      spreadRadius: 0,
+                      offset: const Offset(20, 20),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            /// Bottom Image
             Positioned(
               bottom: -48,
               left: -48,
               right: -48,
               child: Image.asset(
-                'assets/images/bottom_design.png', // Ensure the image is in assets
+                'assets/images/bottom_design.png',
                 fit: BoxFit.cover,
               ),
             ),
+
+            ///Logo
             Center(
               child: TweenAnimationBuilder(
                 duration: const Duration(seconds: 2),
@@ -48,7 +92,7 @@ class _SplashScreenState extends State<SplashScreen> {
                   );
                 },
                 child: Image.asset(
-                  'assets/images/rewaqx_logo.png', // Ensure the logo is in assets
+                  'assets/images/rewaqx_logo.png',
                   width: 200,
                 ),
               ),
