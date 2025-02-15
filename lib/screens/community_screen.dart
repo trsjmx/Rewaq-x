f80f7b999e82228a5a095c586a10be4a8c4894f0
tasneem <tasneemalhattami@gmail.com>
2025-02-15 19:35:17 +0300

    community screens V2

--- lib/screens/community_screen.dart
+++ lib/screens/community_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:rewaqx/screens/create_post.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -17,50 +19,66 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class CommunityScreen extends StatelessWidget {
+class CommunityScreen extends StatefulWidget {
   const CommunityScreen({super.key});
+
+  @override
+  _CommunityScreenState createState() => _CommunityScreenState();
+}
+
+class _CommunityScreenState extends State<CommunityScreen> {
+  // Initialize reactions for posts
+  Map<String, int> post1Reactions = {'❤️': 7, '👍': 2, '🫡': 4, '🏆': 1, '🚀': 1};
+  Map<String, int> post2Reactions = {'❤️': 10, '😍': 5, '🫡': 5, '🔥': 3};
+  Map<String, int> post3Reactions = {'❤️': 7, '👍': 2, '🫡': 4, '🏆': 1, '🚀': 1};
+
+  // Method to update reactions dynamically
+  void _updateReactions(Map<String, int> reactions, String emoji) {
+    setState(() {
+      reactions[emoji] = reactions.containsKey(emoji) ? reactions[emoji]! + 1 : 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFFAFAFC),
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(80.0), // Set the height to your desired value
-        child: AppBar(
-          backgroundColor: Colors.white,           // White background
-          elevation: 0,                             // Remove default shadow
-          title: const Text(
-            'UPM Community',
-            style: TextStyle(
-              fontFamily: 'Quicksand',
-              fontWeight: FontWeight.bold,          // Bold text
-              fontSize: 18,                         // Font size 18
-              color: Color(0xFF7A1DFF),             // Color #7A1DFF
-            ),
-          ),
-          centerTitle: true,                        // Center the title
-          shadowColor: const Color(0x1A1B1D36),     // Shadow color with 10% opacity
-          shape: const RoundedRectangleBorder(
-            side: BorderSide(color: Colors.transparent),
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(0),
-            ),
-          ),
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0x1A1B1D36),          // Shadow color with 10% opacity
-                  offset: Offset(0, 4),              // Position: y = 4
-                  blurRadius: 20,                    // Blur radius: 20
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(80.0), // Set the height to your desired value
+            child: AppBar(
+              backgroundColor: Colors.white,           // White background
+              elevation: 0,                             // Remove default shadow
+              title: const Text(
+                'UPM Community',
+                style: TextStyle(
+                  fontFamily: 'Quicksand',
+                  fontWeight: FontWeight.bold,          // Bold text
+                  fontSize: 18,                         // Font size 18
+                  color: Color(0xFF7A1DFF),             // Color #7A1DFF
                 ),
-              ],
+              ),
+              centerTitle: true,                        // Center the title
+              shadowColor: const Color(0x1A1B1D36),     // Shadow color with 10% opacity
+              shape: const RoundedRectangleBorder(
+                side: BorderSide(color: Colors.transparent),
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(0),
+                ),
+              ),
+              flexibleSpace: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0x1A1B1D36),          // Shadow color with 10% opacity
+                      offset: Offset(0, 4),              // Position: y = 4
+                      blurRadius: 20,                    // Blur radius: 20
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
-      ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -71,7 +89,7 @@ class CommunityScreen extends StatelessWidget {
               time: '2h',
               content:
                   'Just secured the MegaTech partnership! 🌟 This is a huge step forward for our team’s vision and growth goals. Proud of what we’ve accomplished together!',
-              reactions: {'❤️': 7, '👍': 2, '🫡': 4 , '🏆': 1 , '🚀':1},
+              reactions: post1Reactions,
               comments: 6,
             ),
             const SizedBox(height: 16.0),
@@ -81,8 +99,9 @@ class CommunityScreen extends StatelessWidget {
               time: '3h',
               content:
                   'We’re working on a new project — a smart task management system to make work life easier for everyone. Excited to see how this turns out! 🌟 Got ideas? Drop them my way! 😊',
-              reactions: {'❤️': 10, '😍': 5, '🫡': 5 , '🔥': 3 },
+              reactions: post2Reactions,
               comments: 10,
+              imageUrl: 'assets/images/office.png',
             ),
             const SizedBox(height: 16.0),
             _buildPost(
@@ -90,29 +109,138 @@ class CommunityScreen extends StatelessWidget {
               role: 'Marketing Specialist ',
               time: '5h',
               content: 'kjjjk',
-              reactions: {'❤️': 7, '👍': 2, '🫡': 4 , '🏆': 1 , '🚀':1} ,
+              reactions: post3Reactions,
               comments: 8,
             ),
           ],
         ),
       ),
-
       floatingActionButton: FloatingActionButton(
         shape: CircleBorder(),
-        onPressed: () {
-          // Handle the FAB click (e.g., navigate to a new post screen)
-          print('Floating action button pressed!');
+        onPressed: () async {
+          // Navigate to the CreatePostScreen and wait for the result
+          final newPostData = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => CreatePostScreen(),
+            ),
+          );
+
+          // Handle the new post data
+          if (newPostData != null) {
+            final postContent = newPostData['content'];
+            final postImage = newPostData['image'];
+            print('New post: $postContent');
+            if (postImage != null) {
+              print('Post image: $postImage');
+            }
+          }
         },
-        backgroundColor: Color(0xFF2AD2C9), // FAB background color
+        backgroundColor: Color(0xFF2AD2C9),
         child: Icon(
-          Icons.add, // "+" icon
-          color: Colors.white, // Icon color
-        
-        ),          
+          Icons.add,
+          color: Colors.white,
+        ),
       ),
     );
   }
-    
+  
+
+  void _showReactionOverlay(BuildContext context, Map<String, int> reactions) {
+    final List<Map<String, dynamic>> reactionOptions = [
+      {'emoji': '❤️', 'points': 0},
+      {'emoji': '👍', 'points': 10},
+      {'emoji': '👏', 'points': 20},
+      {'emoji': '😍', 'points': 30},
+      {'emoji': '🫡', 'points': 40},
+      {'emoji': '🔥', 'points': 50},
+      {'emoji': '🎁', 'points': 60},
+      {'emoji': '💪', 'points': 70},
+      {'emoji': '🏆', 'points': 80},
+      {'emoji': '🚀', 'points': 90},
+    ];
+
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(16.0)),
+          ),
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Choose Your Reaction',
+                style: TextStyle(
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Quicksand',
+                  color: Color(0xFF1B1D36)
+                  
+                ),
+                
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                'Show your support and appreciation! Each emoji comes with points that will be added to the post owner\'s score.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey[600],
+                  fontFamily: 'Quicksand',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4, // 3 emojis per row
+                  crossAxisSpacing: 16.0,
+                  mainAxisSpacing: 16.0,
+                ),
+                itemCount: reactionOptions.length,
+                itemBuilder: (context, index) {
+                  final reaction = reactionOptions[index];
+                  return GestureDetector(
+                    onTap: () {
+                      // Update reactions on emoji selection
+                      _updateReactions(reactions, reaction['emoji']);
+                      Navigator.pop(context); // Close the overlay
+                    },
+                    child: Column(
+                      children: [
+                        Text(
+                          reaction['emoji'],
+                          style: TextStyle(fontSize: 32.0),
+                        ),
+                        SizedBox(height: 4.0),
+                        Text(
+                          '${reaction['points']} Points',
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.grey[600],
+                            fontFamily: 'Quicksand',
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+              SizedBox(height: 16.0),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+
   Widget _buildPost({
     required String name,
     required String role,
@@ -120,11 +248,12 @@ class CommunityScreen extends StatelessWidget {
     required String content,
     required Map<String, int> reactions,
     required int comments,
+    String? imageUrl,
   }) {
     return Container(
       decoration: BoxDecoration(
         color: Colors.white, // Card background color
-        borderRadius: BorderRadius.circular(8.0), // Rounded corners
+        borderRadius: BorderRadius.circular(10.0), // Rounded corners
         border: Border.all(
           color: Color(0xFFF4F4F4), // Border color
           width: 1.0, // Border thickness
@@ -193,6 +322,19 @@ class CommunityScreen extends StatelessWidget {
             ),
             const SizedBox(height: 8.0),
 
+            // Display image if provided
+            if (imageUrl != null)
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: Image.asset(
+                  imageUrl,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: 200,
+                ),
+              ),
+            const SizedBox(height: 8.0),
+
             // Divider line
             const Divider(
               color: Color(0xFFF4F4F4), // Line color
@@ -211,7 +353,7 @@ class CommunityScreen extends StatelessWidget {
                     width: 18.0,
                   ),
                   onPressed: () {
-                    // Handle add reaction
+                    _showReactionOverlay(context , reactions); 
                   },
                 ),
                 const SizedBox(width: 7.0),
---
+++ lib/screens/create_post.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart'; 
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+
+
+
+class CreatePostScreen extends StatefulWidget {
+  const CreatePostScreen({super.key});
+
+  @override
+  _CreatePostScreenState createState() => _CreatePostScreenState();
+}
+
+class _CreatePostScreenState extends State<CreatePostScreen> {
+  final TextEditingController _postController = TextEditingController();
+   String? _imagePath;
+
+  @override
+  void dispose() {
+    _postController.dispose();
+    super.dispose();
+  }
+  
+// Method to handle image selection
+Future<void> _pickImage() async {
+  final ImagePicker _picker = ImagePicker();
+  
+  // Pick an image from the gallery
+  final XFile? pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+  if (pickedFile != null) {
+    setState(() {
+      _imagePath = pickedFile.path;
+    });
+  } else {
+    // Handle the case when no image is selected
+    print('No image selected');
+  }
+}
+
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFAFAFC),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80.0), // Set the height to your desired value
+        child: AppBar(
+          backgroundColor: Colors.white,           // White background
+          elevation: 0,                             // Remove default shadow
+          title: const Text(
+            'Create Post',
+            style: TextStyle(
+              fontFamily: 'Quicksand',
+              fontWeight: FontWeight.bold,          // Bold text
+              fontSize: 18,                         // Font size 18
+              color: Color(0xFF7A1DFF),             // Color #7A1DFF
+            ),
+          ),
+          leading: IconButton(
+          icon: SvgPicture.asset(
+            'assets/images/chevron-left.svg',
+            width: 24,
+            height: 24,
+          ),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+          centerTitle: true,                        // Center the title
+          shadowColor: const Color(0x1A1B1D36),     // Shadow color with 10% opacity
+          shape: const RoundedRectangleBorder(
+            side: BorderSide(color: Colors.transparent),
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(0),
+            ),
+          ),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0x1A1B1D36),          // Shadow color with 10% opacity
+                  offset: Offset(0, 4),              // Position: y = 4
+                  blurRadius: 20,                    // Blur radius: 20
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _postController,
+              decoration: InputDecoration(
+                hintText: "What's happening?!",
+                hintStyle: TextStyle(
+                  fontFamily: 'Quicksand',
+                  color: Colors.grey[600],
+                ),
+                border: InputBorder.none,
+              ),
+              maxLines: 4,
+              style: TextStyle(
+                fontFamily: 'Quicksand',
+                fontSize: 16.0,
+              ),
+              onChanged: (text) {
+                // Update the state to enable/disable the Share button
+                setState(() {});
+              },
+            ),
+            if (_imagePath != null)
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Image.file(
+                    File(_imagePath!),
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: 200,
+                  ),
+                ),
+              )
+            else
+              const SizedBox.shrink(),
+
+            const Spacer(),
+            Row(
+              children: [
+              // Add Image Button
+              IconButton(
+                onPressed: _pickImage,
+                icon: Icon(
+                  Icons.image,
+                  color: Color(0xFF2AD2C9),
+                ),
+              ),
+                const Spacer(),
+                // Share Button
+                ElevatedButton(
+                  onPressed: _postController.text.trim().isEmpty
+                      ? null // Disable the button if there's no text
+                      : () {
+                          // Handle post creation
+                          final postContent = _postController.text.trim();
+                          if (postContent.isNotEmpty) {
+                            // Navigate back to the community screen with the new post
+                            Navigator.pop(context, {
+                              'content': postContent,
+                              'image': _imagePath,
+                            });
+                          }
+                        },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: _postController.text.trim().isEmpty
+                        ? Colors.grey[300] // Disabled color
+                        : Color(0xFF2AD2C9), // Enabled color
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(22.0),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+                  ),
+                  child: const Text(
+                    'Share',
+                    style: TextStyle(
+                      fontFamily: 'Quicksand',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
