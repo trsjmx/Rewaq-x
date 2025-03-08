c58861d69382d932452d6de79698c70f43cc09b9
Rana2E <4010552@upm.edu.sa>
2025-03-09 00:27:57 +0300

    home page v1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-
+// rana's page
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(250, 250, 252, 1),
+      backgroundColor: const Color.fromARGB(255, 24, 24, 131),
       appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(250, 250, 252, 1),
+        backgroundColor: const Color.fromARGB(255, 173, 19, 101),
         automaticallyImplyLeading: false, // Disable the back arrow
         title: const Text('Home Screen'),
       ),
